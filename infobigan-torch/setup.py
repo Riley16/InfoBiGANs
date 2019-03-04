c978b27f52939ae8084dc91e132184d05d0a11ad
rciric <rc042010@mymail.pomona.edu>
2019-03-03 17:48:44 -0800

    reorganise code

---
+++ infobigan-torch/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='infobigan_torch',
+      version='0.1',
+      description='Torch implementation of InfoBiGAN',
+      url='https://github.com/Riley16/InfoBiGANs/tree/master/infobigan-torch',
+      license='MIT',
+      packages=['infobigan_torch'])
